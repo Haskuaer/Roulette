98347c704c01e8ad0940449db8acb524fe10988c
Haskuaer <haskuaer@gmail.com>
2025-01-19 17:25:18 +0100

    Upgraded game-panel --WORKING

--- client/src/main/java/ef/client/controllers/GameController.java
+++ client/src/main/java/ef/client/controllers/GameController.java
@@ -1,10 +1,13 @@
 package ef.client.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import ef.client.util.SceneManager;
 import ef.client.util.WindowController;
@@ -22,6 +25,8 @@ public class GameController {
     @FXML
     private Button minimizeBtn;
     @FXML
+    private GridPane board;
+    @FXML
     private ImageView token10, token25, token50, token100, token500, token1000;
     @FXML
     private  ImageView[] tokens = new ImageView[6];
@@ -36,12 +41,35 @@ public class GameController {
         this.stage = stage;
         System.out.println("UserPanelController stage: " + stage);
 
+        initializeBoard();
         initializeImages();
 
         //Window functions
         windowController.dragWindow(rootPane, stage);
         windowController.minimizeWindow(stage, minimizeBtn);
         windowController.closeWindow(stage, exitBtn);
+    }
+
+    private void initializeBoard(){
+
+        int cols = 13;
+        int index = 0;
+
+        for(Node node:board.getChildren()){
+            if(node instanceof Pane pane){
+
+                int row = index / cols;
+                int col = index % cols;
+
+                if((row + col) % 2 == 0){
+                    pane.getStyleClass().add("even");
+                } else {
+                    pane.getStyleClass().add("odd");
+                }
+
+                index++;
+            }
+        }
     }
 
     private void initializeImages(){
