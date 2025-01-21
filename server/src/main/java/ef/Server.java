4d8a4eeb578961c22a600ce9db0056c15a4e0531
Haskuaer <haskuaer@gmail.com>
2025-01-21 22:08:04 +0100

    Added login handling with hibernete, creating test user

--- server/src/main/java/ef/Server.java
+++ server/src/main/java/ef/Server.java
@@ -2,22 +2,44 @@ package ef;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ef.actions.LoginRequest;
+import ef.dao.UserDao;
+import ef.models.Users;
 import ef.util.DatabaseConnection;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 import java.io.*;
 import java.net.*;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 
 public class Server {
 
+    private static UserDao userDao;
+
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(12345)) {
 
             System.out.println("Server is running on port 12345...");
+
             Connection db = DatabaseConnection.getConnection();
+
+            SessionFactory factory = new Configuration()
+                    .configure("hibernate.cfg.xml")
+                    .addAnnotatedClass(Users.class)
+                    .buildSessionFactory();
+
+//
+            userDao = new UserDao(factory);
+
+            try{
+                Users test = new Users("test", "test");
+                userDao.addUser(test);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+
 //            try(PreparedStatement preparedStatement = db.prepareStatement("INSERT INTO users (username, password) VALUES (?, ?);")){
 //                preparedStatement.setString(1, "test");
 //                preparedStatement.setString(2, "test");
@@ -62,7 +84,9 @@ public class Server {
 
         System.out.println("Handling login for: " + loginRequest.getUsername());
 
-        if(loginRequest.getUsername().equals("test") && loginRequest.getPassword().equals("test")) { return "success"; }
+        Users user = userDao.getUserByUsername(loginRequest.getUsername());
+
+        if(user != null && user.getPassword().equals(loginRequest.getPassword())) { return "success"; }
         else { return "error"; }
     }
 }
