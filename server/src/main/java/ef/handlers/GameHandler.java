6bc70349a7206a4fd5fdc373af78d3fc77d65d43
Haskuaer <haskuaer@gmail.com>
2025-02-04 21:12:58 +0100

    Added handler for game sessions

--- server/src/main/java/ef/handlers/GameHandler.java
+++ server/src/main/java/ef/handlers/GameHandler.java
@@ -1,2 +1,29 @@
-package ef.handlers;public class GameHandler {
+package ef.handlers;
+
+import ef.services.GameSession;
+
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameHandler {
+
+    private static final List<GameSession> activeSessions = new ArrayList<>();
+    private static final int MAX_SESSIONS = 5;
+
+    public static synchronized GameSession findOrCreateSession()
+    {
+        for(GameSession session : activeSessions)
+        {
+            if(!session.isFull()) { return session; }
+        }
+        if (activeSessions.size() < MAX_SESSIONS)
+        {
+            GameSession newSession = new GameSession();
+            activeSessions.add(newSession);
+            System.out.println("New session created");
+            return newSession;
+        }
+        return null;
+    }
 }
