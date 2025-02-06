b0a7c8e719553f72eb4a3c29f25e0ccd3d20baad
Haskuaer <haskuaer@gmail.com>
2025-02-06 23:18:56 +0100

    Added round dao

--- server/src/main/java/ef/dao/RoundDao.java
+++ server/src/main/java/ef/dao/RoundDao.java
@@ -1,2 +1,38 @@
-package ef.dao;public class RoundDao {
+package ef.dao;
+
+import ef.models.GameSession;
+import ef.models.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import ef.models.Round;
+
+import java.util.UUID;
+
+public class RoundDao {
+
+    private SessionFactory sessionFactory;
+
+    public RoundDao(SessionFactory sessionFactory) { this.sessionFactory = sessionFactory; }
+
+    public Round createRound(User user, GameSession gameSession)
+    {
+        Transaction transaction = null;
+        Round round = new Round();
+
+        try(Session session = sessionFactory.openSession())
+        {
+            transaction = session.beginTransaction();
+            session.save(round);
+            transaction.commit();
+            System.out.println("Round created: " + round.getId());
+        }
+        catch(Exception e)
+        {
+            if(transaction != null) { transaction.rollback(); }
+            e.printStackTrace();
+        }
+        return round;
+    }
 }
