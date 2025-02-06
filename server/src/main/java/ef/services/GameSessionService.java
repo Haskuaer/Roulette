58b63149491a582d4547994775deb6c31c62fa3d
Haskuaer <haskuaer@gmail.com>
2025-02-06 23:25:11 +0100

    Added round creation in started session

--- server/src/main/java/ef/services/GameSessionService.java
+++ server/src/main/java/ef/services/GameSessionService.java
@@ -1,7 +1,10 @@
 package ef.services;
 
 import ef.dao.GameSessionDao;
+import ef.dao.RoundDao;
 import ef.models.GameSession;
+import ef.models.Round;
+import ef.models.User;
 
 import java.net.Socket;
 import java.util.ArrayList;
@@ -11,8 +14,9 @@ import java.util.UUID;
 public class GameSessionService
 {
     private final GameSessionDao gameSessionDao;
+    private final RoundDao roundDao;
 
-    public GameSessionService(GameSessionDao gameSessionDao){ this.gameSessionDao = gameSessionDao; }
+    public GameSessionService(GameSessionDao gameSessionDao, RoundDao roundDao){ this.gameSessionDao = gameSessionDao; this.roundDao = roundDao; }
 
     public GameSession findOrCreateSession()
     {
@@ -22,12 +26,15 @@ public class GameSessionService
         return session;
     }
 
-    public boolean addPlayerToSession(GameSession session)
+    public boolean startSession(User user, GameSession session)
     {
         if(!session.isFull())
         {
             session.setPlayersCount(session.getPlayersCount() + 1);
+            if(session.getRounds().isEmpty()) { Round round = new Round(session); session.addRound(round); }
             if(session.getPlayersCount() == session.getMaxPlayers()) { session.setStatus("active"); }
+            session.addUser(user);
+            session.getRounds();
             gameSessionDao.updateSession(session);
             return true;
         }
