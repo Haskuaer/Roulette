8c6583da7ec15484fcd5d2adce1781b4928ca802
Haskuaer <haskuaer@gmail.com>
2025-02-11 10:04:36 +0100

    Added methods for futhure development

--- client/src/main/java/ef/client/controllers/GameController.java
+++ client/src/main/java/ef/client/controllers/GameController.java
@@ -1,17 +1,11 @@
 package ef.client.controllers;
 
 import ef.client.services.BalanceInfo;
-import ef.client.util.GameStatusHolder;
 import ef.client.util.UserID_Holder;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -25,7 +19,6 @@ public class GameController {
 
     private Stage stage;
     private UUID userId = UserID_Holder.getUserId();
-    private String status = GameStatusHolder.getGameStatus();
     private SceneManager sceneManager;
     private final WindowController windowController = new WindowController();
 
@@ -50,7 +43,7 @@ public class GameController {
         System.out.println("GameController stage: " + stage);
 
         updateBalance();
-        handleOverlay(status);
+        //handleOverlay(status);
 
         //Window functions
         windowController.dragWindow(rootPane, stage);
