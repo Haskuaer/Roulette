c1bd177c884beb0efc6d108564f4efee5d8deb54
Haskuaer <haskuaer@gmail.com>
2025-02-11 09:52:48 +0100

    Added comments

--- server/src/main/java/ef/services/AccountService.java
+++ server/src/main/java/ef/services/AccountService.java
@@ -2,26 +2,23 @@ package ef.services;
 
 import ef.dao.UserDao;
 
-import java.io.IOException;
 import java.util.UUID;
 
 public class AccountService {
 
     private final UserDao userDao;
 
+    //CONSTRUCTOR
     public AccountService(UserDao userDao) { this.userDao = userDao; }
 
+    //GETTERS
     public String getUsername(UUID userId) { return userDao.getUsername(userId); }
-
     public double getBalance(UUID userId) { return userDao.getBalance(userId); }
 
-    public String addFunds(UUID userId, double amount) {
-        try{
-            userDao.setBalance(userId, amount);
-            return "success";
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-            return "error";
-        }
+    //ADD FUNDS TO ACCOUNT
+    public String addFunds(UUID userId, double amount)
+    {
+        try { userDao.setBalance(userId, amount); return "success"; }
+        catch (IllegalAccessException e) { e.printStackTrace(); return "error"; }
     }
 }
