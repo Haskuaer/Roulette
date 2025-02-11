71b2f58c946cd0157a4bd244aa17103a57ed26f0
Haskuaer <haskuaer@gmail.com>
2025-02-11 09:51:42 +0100

    Added coments and improved logs feedback

--- server/src/main/java/ef/services/AuthService.java
+++ server/src/main/java/ef/services/AuthService.java
@@ -11,46 +11,37 @@ public class AuthService {
 
     private final UserDao userDao;
 
-    public AuthService(UserDao userDao){
-        this.userDao = userDao;
-    }
+    //CONSTRUCTOR
+    public AuthService(UserDao userDao){ this.userDao = userDao; }
 
+    //HANDLE LOGIN REQUEST DATA
     public UUID handleLogin(LoginRequest loginRequest) {
-        System.out.println("Handling login for: " + loginRequest.getUsername());
+        System.out.println("SV: Handling login for: " + loginRequest.getUsername());
         try
         {
             User user = userDao.checkCreds(loginRequest.getUsername(), loginRequest.getPassword());
-            if(user == null)
-            {
-                System.out.println("User not found!");
-                return null;
-            }
-            System.out.println("Found user: " + user);
+            if(user == null) { return null; }
             return user.getId();
-        } catch (IllegalAccessError e)
-        {
-            e.printStackTrace();
         }
+        catch (IllegalAccessError e) { e.printStackTrace(); }
         return null;
     }
 
-    public UUID handleRegister(RegisterRequest registerRequest) {
-        System.out.println("Handling register for: " + registerRequest.getUsername());
-
-        try {
+    //HANDLE REGISTER REQUEST DATA
+    public UUID handleRegister(RegisterRequest registerRequest)
+    {
+        System.out.println("SV: Handling register for: " + registerRequest.getUsername());
+        try
+        {
             User user = userDao.getUserByUsername(registerRequest.getUsername());
 
-            if (user != null) {
-                return null;
-            }
+            if (user != null) { return null; }
 
             User newUser = new User(registerRequest.getUsername(), registerRequest.getPassword());
             userDao.addUser(newUser);
             return newUser.getId();
-
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
         }
+        catch (IllegalAccessException e) { e.printStackTrace(); }
 
         return null;
     }
