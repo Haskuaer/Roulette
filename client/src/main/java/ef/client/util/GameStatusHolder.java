bc9dfe257a0637ea48519953d49838d31fe0dec7
Haskuaer <haskuaer@gmail.com>
2025-02-11 09:49:43 +0100

    Added userDao and methods for working with game session status

--- client/src/main/java/ef/client/util/GameStatusHolder.java
+++
@@ -1,9 +0,0 @@
-package ef.client.util;
-
-public class GameStatusHolder
-{
-    private static String gameStatus;
-
-    public static String getGameStatus(){ return gameStatus; }
-    public static void setGameStatus(String gameStatus){ GameStatusHolder.gameStatus = gameStatus; }
-}
--- server/src/main/java/ef/services/GameSessionService.java
+++ server/src/main/java/ef/services/GameSessionService.java
@@ -2,21 +2,20 @@ package ef.services;
 
 import ef.dao.GameSessionDao;
 import ef.dao.RoundDao;
+import ef.dao.UserDao;
 import ef.models.GameSession;
-import ef.models.Round;
-import ef.models.User;
 
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-public class GameSessionService
-{
+public class GameSessionService {
     private final GameSessionDao gameSessionDao;
     private final RoundDao roundDao;
+    private final UserDao userDao;
 
-    public GameSessionService(GameSessionDao gameSessionDao, RoundDao roundDao){ this.gameSessionDao = gameSessionDao; this.roundDao = roundDao; }
+    public GameSessionService(GameSessionDao gameSessionDao, UserDao userDao, RoundDao roundDao)
+    {
+        this.gameSessionDao = gameSessionDao;
+        this.roundDao = roundDao;
+        this.userDao = userDao;
+    }
 
     public GameSession findOrCreateSession()
     {
@@ -29,17 +28,6 @@ public class GameSessionService
 
     public void setSessionStatus(GameSession session)
     {
-        if(session.isFull()){ session.setStatus("active"); }
+        if(session.getMaxPlayers() == 0 && session.getStatus().equals("active")) { session.setStatus("closed"); }
     }
-
-//        if(!session.isFull())
-//        {
-//            if(session.getRounds().isEmpty()) { Round round = new Round(session); session.addRound(round); }
-//            if(session.getPlayersCount() == session.getMaxPlayers()) { session.setStatus("active"); }
-//            session.addUser(user);
-//            session.getRounds();
-//            gameSessionDao.updateSession(session);
-//            return true;
-//        }
-//        return false;
 }
